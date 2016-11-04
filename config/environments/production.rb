c2c59bf96c022bf4421df4ae04e6080fac13ab08
Jarvis Johnson <jarviscjohnson@gmail.com>
2016-11-04 14:34:49 -0400

    Added email config for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,6 +52,23 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
+  # email enabled in production
+  config.action_mailer.smtp_settings = {
+  address: "smtp.sendgrid.net",
+  port: 587,
+  domain: Rails.application.secrets.domain_name,
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: Rails.application.secrets.email_provider_username,
+  password: Rails.application.secrets.email_provider_password
+  }
+  
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "learn-rails_#{Rails.env}"
